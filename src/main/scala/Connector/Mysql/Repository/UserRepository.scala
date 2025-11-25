efe58a30bb25e27ca57180c34a062fbb57b370fe
AnhQuanengineer <aquan0809@gmail.com>
2025-11-25 15:39:22 +0700

    #13 - anhquan write and validate data with spark

--- src/main/scala/Connector/Mysql/Repository/UserRepository.scala
+++ src/main/scala/Connector/Mysql/Repository/UserRepository.scala
@@ -10,4 +10,6 @@ trait UserRepository {
   def findById(user_id: Long): DBIO[Option[User]]
   def update(user: User): DBIO[Int]
   def delete(user_id: Long): DBIO[Int]
+  def addColumn(tableName: String, columnName: String, columnType: String): DBIO[Int]
+  def dropColumn(tableName: String, columnName: String): DBIO[Int]
 }
